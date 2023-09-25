28696d733fb25b088acb76d80d1f56e567f047d6
Pietromuresu <muresupietro66@gmail.com>
2023-09-25 11:47:23 +0200

    Milestone 1

---
+++ src/org/java/eventHandler/Event.java
@@ -0,0 +1,100 @@
+package org.java.eventHandler;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class Event {
+	
+	private String title;
+	private LocalDate date;
+	private int numSeats;
+	private int reservedSeats;
+	
+	public Event(String title, String date, int numSeats) throws Exception {
+		setTitle(title);
+		setDate(date);
+		setNumSeats(numSeats);
+		setReservedSeats(0);
+	}
+	
+	
+	
+	
+	// Getters and setters 
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(String date) throws Exception {
+		
+		LocalDate now = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");	
+		LocalDate typedDate = LocalDate.parse(date, formatter);
+		
+        String formattedDate = typedDate.format(formatter);
+		
+		if(typedDate.isBefore(now))
+			throw new Exception("La data inserita non è valida : Data già passata");
+		
+		this.date = typedDate;
+	}
+
+	public int getNumSeats() {
+		return numSeats;
+	}
+
+	private void setNumSeats(int numSeats) throws Exception {
+		
+		if(numSeats <= 0)
+			throw new Exception("Il numero dei posti non può essere negativo o uguale a 0(zero)");
+		
+		this.numSeats = numSeats;
+	}
+
+	public int getReservedSeats() {
+		return reservedSeats;
+	}
+
+	private void setReservedSeats(int reservedSeats) {
+		this.reservedSeats = reservedSeats;
+	}
+	
+	
+	// utils
+	
+	
+	// Prenota se ci sono posti disponibili e se l'evento deve ancora iniziare 
+	public void prenota() throws Exception {
+		if(reservedSeats == numSeats || !date.isAfter(LocalDate.now()))
+			throw new Exception("Impossibile prenotare un posto per questo evento");
+		
+		this.reservedSeats += 1;
+	}
+	
+	
+	public void disdici() throws Exception {
+		if(reservedSeats == 0 || !date.isAfter(LocalDate.now()))
+			throw new Exception("Impossibile disdire la prenotazione");
+		
+		this.reservedSeats -= 1;
+	}
+	
+	@Override
+	public String toString() {
+				
+		return this.date + " - " + this.title;
+	}
+	
+	
+}
--- src/org/java/eventHandler/Main.java
+++ src/org/java/eventHandler/Main.java
@@ -1,5 +1,29 @@
 package org.java.eventHandler;
 
 public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+	
+	Event mioEvento = new Event("Pietro", "2023-09-26", 100);
+	
+		System.out.println(mioEvento);
+		
+		mioEvento.prenota();
+		mioEvento.prenota();
+		mioEvento.prenota();
 
+		
+		mioEvento.disdici();
+		mioEvento.disdici();
+		mioEvento.disdici();
+		
+//		mioEvento.disdici();
+//		mioEvento.disdici();
+		
+		
+		
+		System.out.println(mioEvento.getReservedSeats());
+	}
+	
 }
